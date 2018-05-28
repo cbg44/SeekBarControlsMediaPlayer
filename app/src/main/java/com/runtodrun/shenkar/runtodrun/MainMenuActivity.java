7a957720215e47c7f9d7fe3f13701405b0c8c9e1
dormalk <dormalk@gmail.com>
2018-05-28 17:40:27 +0300

    Add animation background

--- app/src/main/java/com/runtodrun/shenkar/runtodrun/MainMenuActivity.java
+++ app/src/main/java/com/runtodrun/shenkar/runtodrun/MainMenuActivity.java
@@ -3,6 +3,9 @@ package com.runtodrun.shenkar.runtodrun;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -16,5 +19,10 @@ public class MainMenuActivity extends AppCompatActivity {
         overlay.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 | View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+        ImageView spaceshipImage = (ImageView) findViewById(R.id.imageView);
+        Animation hyperspaceJumpAnimation = AnimationUtils.loadAnimation(this, R.anim.background_motion);
+        spaceshipImage.startAnimation(hyperspaceJumpAnimation);
+
     }
 }
