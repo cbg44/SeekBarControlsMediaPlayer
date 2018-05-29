72319de4a5045eac16db800f7a67b10d4bb9f03b
dormalk <dormalk@gmail.com>
2018-05-29 18:45:55 +0300

    create backround animation on strip_view_game

--- app/src/main/java/com/runtodrun/shenkar/runtodrun/GameStripActivity.java
+++ app/src/main/java/com/runtodrun/shenkar/runtodrun/GameStripActivity.java
@@ -1,8 +1,13 @@
 package com.runtodrun.shenkar.runtodrun;
 
+import android.animation.ValueAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
 
 public class GameStripActivity extends AppCompatActivity {
 
@@ -16,5 +21,26 @@ public class GameStripActivity extends AppCompatActivity {
         overlay.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+    private void initBackgroundMotion(){
+        final ImageView backgroundOne = (ImageView) findViewById(R.id.background_one);
+        final ImageView backgroundTwo = (ImageView) findViewById(R.id.background_two);
+
+        final ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setDuration(120000L);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                final float progress = (float) animation.getAnimatedValue();
+                final float width = backgroundOne.getWidth();
+                final float translationX = width * progress;
+                backgroundOne.setTranslationX(-translationX);
+                backgroundTwo.setTranslationX(-(translationX - width));
+            }
+        });
+        animator.start();
     }
 }
--- app/src/main/java/com/runtodrun/shenkar/runtodrun/MainMenuActivity.java
+++ app/src/main/java/com/runtodrun/shenkar/runtodrun/MainMenuActivity.java
@@ -1,14 +1,15 @@
 package com.runtodrun.shenkar.runtodrun;
 
+import android.animation.ValueAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 
 public class MainMenuActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,10 +20,6 @@ public class MainMenuActivity extends AppCompatActivity {
         overlay.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 | View.SYSTEM_UI_FLAG_FULLSCREEN);
-
-        ImageView spaceshipImage = (ImageView) findViewById(R.id.imageView);
-        Animation hyperspaceJumpAnimation = AnimationUtils.loadAnimation(this, R.anim.background_motion);
-        spaceshipImage.startAnimation(hyperspaceJumpAnimation);
 
     }
 }
